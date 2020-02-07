6811ac5224766723a919a36b1bc96a6366f9d2ea
Ilya Zavyalov <ilyuzhaz@gmail.com>
2020-02-07 18:03:46 +0300

    Окна; Их перемещение, ресайз и текстовое наполнение

---
+++ public/classes/consoleWindow.js
@@ -0,0 +1,136 @@
+const Edges = {
+    TOP: 0,
+    LEFT: 1,
+    RIGHT: 2,
+    BOTTOM: 3,
+};
+
+// EdgeGrabDistance - дистанция в пикселях, с которой можно начать "захват" на изменение размера окна
+const EGD = 8;
+// TopBarHeight - высота полоски статуса окна
+const TBH = 30;
+// ButtonSize - ширина и высота кнопок на полоске статуса
+const BS = TBH;
+// TextPadding - ширина и высота отступа текста от краёв окна
+const TP = 10;
+
+class ConsoleWindow {
+    constructor(x, y, width, height, title, stroke) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+        this.title = title || "Window";
+        this.stroke = stroke || color(255, 255, 255);
+    }
+
+    draw() {
+        stroke(this.stroke);
+        strokeWeight(4);
+
+        /*
+                  (1)
+                ------
+            (2) |    | (3)
+                ------
+                  (4)
+        */
+        fill(0);
+        rect(this.x, this.y, this.width, this.height);
+
+        line(this.x, this.y, this.x + this.width, this.y);                              // (1)
+        line(this.x, this.y, this.x, this.y + this.height);                             // (2)
+        line(this.x + this.width, this.y, this.x + this.width, this.y + this.height);   // (3)
+        line(this.x, this.y + this.height, this.x + this.width, this.y + this.height);  // (4)
+
+
+        /*
+            ____________________
+            |    Title(2)    |x| (3)
+            --------------------
+                    (1)
+        */
+
+        line(this.x, this.y + TBH, this.x + this.width, this.y + TBH);  // (1)
+
+        /* (2) */
+        push();
+        textSize(20);
+        textAlign(CENTER, CENTER);
+        fill(255);
+        strokeWeight(3);
+        stroke(10);
+        text(this.title, this.x, this.y, this.width, TBH);
+        pop();
+
+        /* (3) */
+
+        // Разделитель
+        const endX = this.x + this.width;
+        line(endX - BS, this.y, endX - BS, this.y + BS);
+        // Крестик
+        strokeWeight(1);
+        line(endX - BS + 5, this.y + 5, endX - 5, this.y + BS - 5);     // Обратная черта
+        line(endX - 5, this.y + 5, endX - BS + 5, this.y + BS - 5);     // Прямая черта
+
+        /*
+            --------------
+            | some text  |
+            | content    |
+            --------------
+        */
+
+        push();
+        textSize(15);
+        fill(255);
+        strokeWeight(2);
+        stroke(10);
+        text(this.content, this.x + TP, this.y + TP + TBH, this.width - 2*TP, this.height - 2*TP - TBH);
+        pop();
+    }
+
+    mouseCheck(checkX, checkY) {
+        const point = new Point(checkX, checkY);
+        const rX = this.x, rY = this.y, w = this.width, h = this.height;
+
+        // isInteractable - если можно менять размер окна или перемещать его
+        let result = {isInteractable: false, onEdges: [], overCross: false, overInsides: false};
+        // Взаимодействовать можно за верхнюю полоску (без кнопки закрытия)
+        result.isInteractable = point.isInsideRect(rX, rY, w - BS, TBH);
+
+        // Мышка на кресте
+        result.overCross = point.isInsideRect(rX + w - BS, rY, BS, BS);
+
+        // Мышь внутри рабочей части окна
+        result.overInsides = point.isInsideRect(rX, rY + TBH, w, h - TBH);
+
+        // Мышь в небольшой границе от краёв
+        result.onEdges[Edges.TOP]    = point.isInsideRect(rX, rY - EGD, w, EGD);
+        result.onEdges[Edges.LEFT]   = point.isInsideRect(rX - EGD, rY, EGD, h);
+        result.onEdges[Edges.RIGHT]  = point.isInsideRect(rX + w, rY, EGD, h);
+        result.onEdges[Edges.BOTTOM] = point.isInsideRect(rX, rY + h, w, EGD);
+
+        // Если мы захватили хотя бы одну границу - это считается за isInteractable
+        for (let edge in result.onEdges)
+            result.isInteractable = result.isInteractable || result.onEdges[edge];
+
+        return result;
+    }
+}
+
+
+// HELPER:
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    isInsideRect(rectX, rectY, width, height) {
+        return (this.x >= rectX  &&  this.y >= rectY  &&
+                this.x <= (rectX + width) && this.y <= (rectY + height));
+    }
+}
--- public/index.js
+++ public/index.js
@@ -1,13 +1,163 @@
-function setup() {
-    createCanvas(800, 600);
-    fill(255);
-    noStroke();
+/*function swap(arr, ind1, ind2) {
+    let t = arr[ind1];
+    arr[ind1] = arr[ind2];
+    arr[ind2] = t;
+}*/
+
+let consoleFont, titleFont, narratorFont;
+function preload() {
+    consoleFont = titleFont = narratorFont = loadFont("fonts/Inconsolata/Inconsolata-Regular.ttf");
 }
 
+const consoleWindows = [];
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    consoleWindows.push(new ConsoleWindow(100, 100, 300, 300, "Window #1"));
+    consoleWindows.push(new ConsoleWindow(450, 450, 300, 300, "Window #2"));
+    consoleWindows.push(new ConsoleWindow(700, 450, 300, 300, "Window #3"));
+}
+
+const DragMode = {
+    MOVE: 0,
+    RESIZE: 1
+};
+let dragging = {mode: DragMode.MOVE, onEdges: []};
+let grabbedWindow = null;
+let selectedWindow = null;
 function draw() {
     background(0);
-    textAlign(CENTER, CENTER);
-    textSize(30);
-    text("Hello World!", width / 2, 100);
-    circle(mouseX, mouseY, 50);
+
+    let atLeastOneEdgeFound = false;
+    let atLeastOneSelected = false;
+    for (let i = 0; i < consoleWindows.length; i++) {
+        const cw = consoleWindows[i];
+        // Анализируем, где находится мышь (на краях/внутри/снаружи)
+        if (grabbedWindow == null) {
+            let result = cw.mouseCheck(mouseX, mouseY);
+
+            // Сначада проверка на края
+            for (let edge in result.onEdges) {
+                if (result.onEdges[edge] == true) {
+                    // Есть нахождение на границе edge.
+                    if (edge == Edges.LEFT || edge == Edges.RIGHT) cursor("ew-resize");
+                    else cursor("ns-resize");
+
+                    atLeastOneEdgeFound = true;
+                    dragging.mode  = DragMode.RESIZE;
+                    dragging.edges = result.onEdges;
+                }
+            }
+
+            if (result.overCross && mouseIsPressed) {
+                cw.stroke = color(100, 200, 100);
+            }
+
+            if (result.overInsides && mouseIsPressed && !atLeastOneSelected) {
+                if (selectedWindow != null)
+                    selectedWindow.stroke = color(255);
+
+                selectedWindow = cw;
+                cw.stroke = color(100, 100, 200);
+                atLeastOneSelected = true;
+            }
+
+            if (result.isInteractable && mouseIsPressed) {
+                // У последнего окна должен быть выше приоритет для дальнейшего захвата
+                // Перемещаем его из своего место на первое, всё остальное сдвигаем
+                let temp = consoleWindows[i];
+                consoleWindows.splice(i, 1);
+                consoleWindows.unshift(temp);
+
+                grabbedWindow = cw;
+                cw.stroke = color(200, 100, 100);
+            }
+        }
+    }
+
+    if (!atLeastOneEdgeFound && !grabbedWindow) {
+        cursor('default');
+        dragging.mode = DragMode.MOVE;
+    }
+
+    if (!atLeastOneSelected && mouseIsPressed) {
+        if (selectedWindow)
+            selectedWindow.stroke = color(255);
+        selectedWindow = null;
+    }
+
+    // Рисуем в обратном порядке (окна первее в массиве должны рисоваться выше)
+    for (let i = consoleWindows.length - 1; i >= 0; i--)
+        consoleWindows[i].draw();
+}
+
+function windowResized() {
+    const pWidth = width, pHeight = height;
+    resizeCanvas(windowWidth, windowHeight);
+
+    const xRatio = width / pWidth;
+    const yRatio = height / pHeight;
+    for (const cw of consoleWindows) {
+        cw.x *= xRatio;
+        cw.y *= yRatio;
+
+        cw.width *= xRatio;
+        cw.height *= yRatio;
+    }
+}
+
+function mouseDragged() {
+    const gW = grabbedWindow;
+
+    if (gW) {
+        const dX = (mouseX - pmouseX), dY = (mouseY - pmouseY);
+        if (dragging.mode == DragMode.MOVE) {
+            gW.x += dX;
+            gW.y += dY;
+        }
+        else {
+            if (dragging.edges[Edges.TOP]) {
+                gW.height -= dY;
+                gW.y += dY;
+            }
+            if (dragging.edges[Edges.BOTTOM])
+                gW.height += dY;
+
+            if (dragging.edges[Edges.LEFT]) {
+                gW.width -= dX;
+                gW.x += dX;
+            }
+            if (dragging.edges[Edges.RIGHT])
+                gW.width += dX;
+
+            // Убеждаемся, что параметры окна в норме
+            if (gW.width < TBH + 20) gW.width = TBH + 20;
+            if (gW.height < TBH + 20) gW.height = TBH + 20;
+            if (gW.x < 0) gW.x = 0;
+            if (gW.y < 0) gW.y = 0;
+        }
+    }
+}
+
+function mouseReleased() {
+    if (grabbedWindow != null) {
+        grabbedWindow.stroke = color(255);
+        grabbedWindow = null;
+    }
+}
+
+function keyTyped() {
+    let char;
+    if (selectedWindow) {
+        /* Key - строка; a, b, c, Enter, Shift... */
+
+        // Если это не обычные клавиши или Enter/Backspace - игнорируем
+        if (key.length > 0 && key != "Enter" && key != "Backspace") return;
+
+        if (key == "Enter") char = '\n';
+        else char = key;
+
+        if (key != "Backspace") selectedWindow.content += char;
+        else selectedWindow.content =
+            selectedWindow.content.substr(0, selectedWindow.content.length - 1);
+    }
 }
